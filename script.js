c828f82d65f169b318238ccf391fd57523607080
shreyash2509 <shreyash02509@gmail.com>
2023-02-15 00:35:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+(function () {
+  [...document.querySelectorAll(".control")].forEach((button) => {
+    button.addEventListener("click", function () {
+      document.querySelector(".active-btn").classList.remove("active-btn");
+      this.classList.add("active-btn");
+      document.querySelector(".active").classList.remove("active");
+      document.getElementById(button.dataset.id).classList.add("active");
+    });
+  });
+  document.querySelector(".theme-btn").addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+  });
+})();
+
+// var typed=new Typed(".typing",{
+//   strings:["Web Developer","Web Desinger"],
+//   typespeed:100,
+//   backspeed:100,
+//   loop:true
+// })
+
